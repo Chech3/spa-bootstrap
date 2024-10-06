bdb269bcc1832cdfbe5ae4bfed60f8b5cbd0c7e7
Chech3 <joseangelgarces123@gmail.com>
2024-10-06 02:15:49 -0400

    Solo faltan pequenos detalles, pero esta casi listo

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var navbar = document.querySelector('.show-shadow');
+    
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
